6fcb40eaa05ae970ac5f2789594b29aaa580013d
Anton Omelchuk <Anton@iOSs-iMac-2.local>
2020-08-31 21:48:19 +0300

    hash table implementation

---
+++ hash-table.js
@@ -0,0 +1,62 @@
+function hashStringToInt(key, tableSze) {
+  let hash = key.charCodeAt(0)
+
+  for (let i = 0; i < key.length; i++) {
+    hash = (hash * key.charCodeAt(i)) % tableSze
+  }
+
+  return hash
+}
+
+class HashTable {
+  table = new Array(9999)
+  numItems = 0
+
+  resize = () => {
+    const newTable = new Array(this.table.length * 2)
+
+    this.table.forEach(item => {
+      if (item) {
+        item.forEach(([key, value]) => {
+          const idx = hashStringToInt(key, newTable.length)
+
+          if (newTable[idx]) {
+            newTable[idx] .push([key, value])
+          } else {
+            newTable[idx] = [[key, value]]
+          }
+        })
+      }
+    })
+    this.table = newTable
+  }
+
+  setItem = (key, value) => {
+    this.numItems++
+    const idx = hashStringToInt(key, this.table.length)
+
+    loadFactor = this.numItems / this.table.length
+
+    if (loadFactor > .7) {
+      this.resize()
+    }
+
+    if (this.table[idx]) {
+      this.table[idx] .push([key, value])
+    } else {
+      this.table[idx] = [[key, value]]
+    }
+  }
+
+  getItem = (key) => {
+    const idx = hashStringToInt(key, this.table.length)
+
+    if (!this.table[idx]) {
+      return null
+    }
+
+    return this.table[idx].find(x => x[0] === key)[1]
+  }
+}
+
+const hashTable = new HashTable()
